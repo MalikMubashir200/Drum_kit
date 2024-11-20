ed0294dd184d9d76a4bd804530d509487f3081f1
Malik Mubashir <[m.mubashir.dev@gmail.com]>
2024-11-20 05:41:03 -0800

    feat: Add interactive Drum Kit game with keyboard controls and realistic drum sounds

---
+++ index.js
@@ -0,0 +1,69 @@
+var numberOfButtons = document.querySelectorAll("button").length;
+
+for (var index = 0; index < numberOfButtons; index++) {
+    document.querySelectorAll(".drum")[index].addEventListener("click", function () {
+        
+        var buttonInnerHTML = this.innerHTML;
+        keypress(buttonInnerHTML)
+        buttonsAnimation(buttonInnerHTML)
+       
+    });
+}
+
+
+
+document.addEventListener("keypress",function (event) {
+    keypress(event.key)
+    buttonsAnimation(event.key)
+   
+})
+function keypress(event) {
+    switch (event) {
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "l":
+            var bass = new Audio('sounds/kick-bass.mp3');
+            bass.play();
+            break;
+        default:
+            console.log(event); 
+            break;
+    }
+   
+}
+ 
+function buttonsAnimation(currentKey){
+
+    var activeButtons = document.querySelector('.'+currentKey)
+    activeButtons.classList.add('pressed')  ;
+    setTimeout(function () {
+
+        activeButtons.classList.remove('pressed')
+        
+    },100)
+}
+
+
+
